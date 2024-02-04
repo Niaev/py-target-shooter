38b74881a24241087b1b27dc0a474b6ddaea2bbe
Niaev <trutnv@protonmail.com>
2024-02-04 15:18:07 -0300

    Updated index with first working gameplay -- no loosing at all btw

--- index.py
+++ index.py
@@ -6,12 +6,16 @@
 # Specially the Chimp one:
 # https://www.pygame.org/docs/tut/ChimpLineByLine.html
 
+# General Imports
+from random import randint
+
 # PyGame Imports
 import pygame as pg
 
 # Project Imports
 from classes.crosshair import CrossHair
 from classes.hud import Hud
+from classes.targets import BigTarget, MediumTarget, SmallTarget, SuperSmallTarget
 
 # Setup game
 pg.init()
@@ -35,25 +39,74 @@ ch = CrossHair((0,0))
 hud = Hud()
 
 hud_already_loaded = 0
+targets_on_screen = []
+screen_limits = [(30, width-30), (78, height-30)]
+rng_t = [1,1,1,1,2,2,2,3,3,4]
+targets_handler = {
+    '1': BigTarget,
+    '2': MediumTarget,
+    '3': SmallTarget,
+    '4': SuperSmallTarget
+}
 
 while run:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            # Check Mouse Button 1 click
+            mouse_presses = pg.mouse.get_pressed()
+            if mouse_presses[0]:
+                target = ch.shoot(targets_on_screen)
+                if target: # Check if a Target is shot
+                    # Update points and remove target from screen
+                    hud.update_score(screen, target.points)
+                    targets_on_screen.remove(target)
 
+    # Filling the screen with up to 10 targets
+    if len(targets_on_screen) < 2:
+        rng = rng_t[randint(0, len(rng_t)-1)]
+        ChosenTarget = targets_handler[str(rng)]
+
+        # Make sure a new target won't collide with an existing target
+        perfect = False
+        while not perfect:
+            x = randint(*screen_limits[0])
+            y = randint(*screen_limits[1])
+            t = ChosenTarget((x, y))
+
+            collisions = 0
+            for target in targets_on_screen:
+                co = target.collide(t)
+                if co:
+                    collisions += 1
+            
+            if collisions == 0:
+                perfect = True
+        
+        # If it is ok, we have a new target
+        targets_on_screen.append(t)
+
+    # Check if any target time on screen is over
+    for target in targets_on_screen:
+        if target.time_over():
+            targets_on_screen.remove(target)
+
+    # Initiate screen
     screen.fill('grey')
+    # Show Hud
     if not hud_already_loaded:
         hud.draw(screen)
 
-    # Load crosshair and make it follow the mouse
-    ss = pg.sprite.RenderPlain((ch))
+    # Load crosshair and targets
+    ss = pg.sprite.RenderPlain((*targets_on_screen, ch))
     ss.update()
     ss.draw(screen)
 
+    # Update hud time counter
     hud.update_time(screen)
 
     pg.display.flip()
-
     clock.tick(60)
 
 pg.quit()
